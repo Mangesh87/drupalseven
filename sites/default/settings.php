341db18bf93fae25350b4d29e70fab127840750d
mridul <mangesh.p@srijan.com>
2014-09-04 15:53:59 +0530

    Adding new module created through Features Module

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -215,7 +215,7 @@ $databases = array (
   array (
     'default' => 
     array (
-      'database' => 'drupaldb',
+      'database' => 'drupalgit',
       'username' => 'root',
       'password' => 'server',
       'host' => 'localhost',
@@ -256,7 +256,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = 'ScebDZqbxvDjzop_nQkIkSbwMj1IpA8HElArUX6Lhm8';
+$drupal_hash_salt = 'OYGkmmzpUeILwvl01gzBh4Z-lQyfbQocbzguPjyKKZY';
 
 /**
  * Base URL (optional).
